a3d1f8e2d837b8d055a0bcc66de43c9010f38aee
Mathew Dominic Puthoor <mathew.puthur@gmail.com>
2019-06-03 14:00:57 +0530

    Changes to add new stats

--- fantasy_analyzer.js
+++ fantasy_analyzer.js
@@ -1,12 +1,13 @@
 var allInfo = "https://fantasy.premierleague.com/drf/bootstrap-static";
-var id = "1536682";  // Login to fantasy and click on points tab. Replace this variable with id u find in the url
+var id = "107827";  // Login to fantasy and click on points tab. Replace this variable with id u find in the url
+var sorted_players = [];
 $.get(allInfo, function(allInfo) {
 	var all_players = allInfo["elements"];
 	var players = {};
 	var gw_players = [];
-	var sorted_players = [];
+	
 	var j = 0;
-	var lastWeek = 32;  // Change value to last game week value
+	var lastWeek = 38;  // Change value to last game week value
 	function fetchNextGw() {
 		j++;
 		var uri="https://fantasy.premierleague.com/drf/entry/" + id + "/event/" + j.toString() + "/picks"
@@ -16,9 +17,10 @@ $.get(allInfo, function(allInfo) {
 		    	// console.log("players",data["picks"][i]["element"]);
 		    	gw_players.push(data["picks"][i]["element"]);
 		    	if(!(data["picks"][i]["element"] in players)) {
-		    		players[data["picks"][i]["element"]] = [0,1];
+		    		players[data["picks"][i]["element"]] = { "points":0, "num_of_appearances":1, "gameweeks_in_team": [j]};
 		    	} else {
-		    		players[data["picks"][i]["element"]][1]++;	
+		    		players[data["picks"][i]["element"]]["num_of_appearances"]++;
+					players[data["picks"][i]["element"]]["gameweeks_in_team"].push(j)	
 		    	}
 		    }
 		    var uri2="https://fantasy.premierleague.com/drf/event/" + j.toString() + "/live"
@@ -26,18 +28,18 @@ $.get(allInfo, function(allInfo) {
 			$.get(uri2, function(data2) {
 				for(var i in gw_players) {
 					var filter = $.grep(all_players, function(ele) { return ele.id==gw_players[i]; });
-					players[gw_players[i]][0] += data2["elements"][gw_players[i]]["stats"]["total_points"];
+					players[gw_players[i]]["points"] += data2["elements"][gw_players[i]]["stats"]["total_points"];
 		        	console.log(filter[0]["web_name"],data2["elements"][gw_players[i]]["stats"]["total_points"]);
 		        }
 		        if(j < lastWeek) {   
 		        	gw_players = [];
-		        	setTimeout(fetchNextGw, 2000);
+		        	setTimeout(fetchNextGw, 500);
 		        }
 		        else {
 		        	for(var player in players) {
 		        		var filter = $.grep(all_players, function(ele) { return ele.id==parseInt(player); });
-		        		players[player].push(players[player][0]/players[player][1]);
-		        		sorted_players.push({"Name":filter[0]["web_name"],"Total Points earned for u":players[player][0],"No of times in team":players[player][1],"Avg score":players[player][2]});
+		        		players[player]["avg_points"] = players[player]["points"]/players[player]["num_of_appearances"];
+		        		sorted_players.push({"Name":filter[0]["web_name"],"Total Points earned for u":players[player]["points"],"No of times in team":players[player]["num_of_appearances"],"Avg score":players[player]["avg_points"], "Gameweek Appearences": players[player]["gameweeks_in_team"]});
 		        	}
 		        	sorted_players.sort(function(a,b) { 
 		        		return b["Avg score"]-a["Avg score"]; // Change "Avg score" to "Total Points earned for u" or "No of times in team" to print in that order
@@ -48,5 +50,5 @@ $.get(allInfo, function(allInfo) {
 			
 		});
 	}	
-	setTimeout(fetchNextGw, 2000);
+	setTimeout(fetchNextGw, 500);
 });
